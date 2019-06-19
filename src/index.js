1f80cef848cfcdb5c0ad2f3dd1c87fcffe81b2ae
lakhansamani <lakhan.m.samani@gmail.com>
2019-06-19 11:31:41 +0530

    fix: navbar export

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export mediaQueries from './media-queries';
 export DesignKitThemeProvider from './components/DesignKitThemeProvider';
 export Flex from './components/Flex';
 export Logo from './components/Logo';
+export NavBar from './components/NavBar';
